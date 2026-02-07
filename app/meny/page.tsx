3ace920dbcd8d8cf64cf25ccd1f4088f1f22a176
Egeeoz <ozakdemirege@hotmail.com>
2026-02-07 15:11:13 +0100

    Update menu content

--- app/meny/page.tsx
+++ app/meny/page.tsx
@@ -8,23 +8,132 @@ function page() {
 
         <div className="flex">
           <div>
-            <h3>Marinara</h3>
-            <h3>Capricciosa</h3>
-            <h3>Al Tonno</h3>
-            <h3>Il Gallo</h3>
-            <h3>Prezzemolo</h3>
-            <h3>Frutti di Mare</h3>
-            <h3>Pizza Lovers Delight - Pizzamackan</h3>
-            <h3>Pizza kit</h3>
+            <article>
+              <h3>Marinara</h3>
+              <p>Tomatsås, färsk vitlök, oregano och färsk basilika.</p>
+              <p>115kr</p>
+            </article>
+
+            <article>
+              <h3>Capricciosa</h3>
+              <p>
+                Tomatsås, rökt skinka, färska champinjoner, mozzarella,
+                parmesan, kronärtskocka och färsk basilika.
+              </p>
+              <p>140kr</p>
+            </article>
+
+            <article>
+              <h3>Al Tonno</h3>
+              <p>
+                Tomatsås, parmesan, Mozzarella, tonfisk, oliver, kapris, röd lök
+                och oregano.
+              </p>
+              <p>140kr</p>
+            </article>
+
+            <article>
+              <h3>Il Gallo</h3>
+              <p>
+                Tomatsås, kycklingfilé, mozzarella, oliver, färsk paprika och
+                tryffelsås.
+              </p>
+              <p>140kr</p>
+            </article>
+
+            <article>
+              <h3>Prezzemolo</h3>
+              <p>
+                Tomatsås, mozzarella, parmesan, handskalade räkor, färsk vitlök,
+                och färsk persilja.
+              </p>
+              <p>155kr</p>
+            </article>
+            <article>
+              <h3>Frutti di Mare</h3>
+              <p>
+                Tomatsås, mozzarella, parmesan, handskalade räkor, Blå musslor,
+                färsk vitlök, färsk persilja och citronskiva.
+              </p>
+              <p>165kr</p>
+            </article>
+
+            <article>
+              <h3>Pizza Lovers Delight - Pizzamackan</h3>
+              <p>100kr</p>
+            </article>
+
+            <article>
+              <h3>Pizza kit</h3>
+              <p>
+                Amores pizza deg (48 timar jäsning) 280 g, vår här gjort
+                tomatsås, mozzarella ost, parmigiano ost, i vikt som vi använder
+                när vi bakar våra pizzor.
+              </p>
+              <p>69kr</p>
+            </article>
           </div>
           <div>
-            <h3>Margherita</h3>
-            <h3>Cacciatora</h3>
-            <h3>Vegana</h3>
-            <h3>Mille Amore</h3>
-            <h3>Estate</h3>
-            <h3>Mezaluna halvinbakad</h3>
-            <h3>Kebab pizza</h3>
+            <article>
+              <h3>Margherita</h3>
+              <p>
+                Tomatsås, parmesan, mozzarella, oregano och färsk basilika med
+                en smak av våra här gjord gelato.
+              </p>
+              <p>125kr</p>
+            </article>
+
+            <article>
+              <h3>Cacciatora</h3>
+              <p>
+                Tomatsås, mozzarella, Ventricina pikante (stark salami med
+                mycket smak), parmesan, röd lök och färsk basilika.
+              </p>
+              <p>140kr</p>
+            </article>
+            <article>
+              <h3>Vegana</h3>
+              <p>
+                Tomatsås, oliver, grillad paprika tricolore, färska
+                champinjoner, rödlök, stark grillad paprika röra, oregano och
+                hickory grovsalt.
+              </p>
+              <p>145kr</p>
+            </article>
+
+            <article>
+              <h3>Mille Amore</h3>
+              <p>
+                Mild ost, parmesan, gorgonzola, mozzarella, prosciutto, torkad
+                tomat och pepparmix.
+              </p>
+              <p>155kr</p>
+            </article>
+
+            <article>
+              <h3>Estate</h3>
+              <p>
+                Tomatsås, Mozzarella, ruccola, prosciutto, parmesan, pesto,
+                balsamico kräm och pinjenötter.
+              </p>
+              <p>160kr</p>
+            </article>
+            <article>
+              <h3>Mezaluna halvinbakad</h3>
+              <p>
+                Tomatsås, rökt skinka, mozzarella, ruccolasallad, parmesan,
+                prosciutto och pinjenötter.
+              </p>
+              <p>155kr</p>
+            </article>
+            <article>
+              <h3>Kebab pizza</h3>
+              <p>
+                Tomatsås, ost, kebabkött, färsk tomat, färsk rödlök, feferoni,
+                vitlöksdressing och kebabdressing.
+              </p>
+              <p>155kr</p>
+            </article>
           </div>
         </div>
       </section>
